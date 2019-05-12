a55a4fe7dc22994cc8e9fc1c41e4d9b08b9173cc
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-05-12 14:18:21 -0500

    prop types | add proptypes Meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Meme = ({ name }) => <h2>{name}</h2>
+
+Meme.propTypes = {
+  name: PropTypes.string.isRequired
+}
 
 export default Meme
