3310e21019bf38bd7cfdb67923e41b892960d194
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-05-12 14:02:50 -0500

    type of components | MemeContainer component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Meme from './Meme';
+import MemeContainer from './components/MemeContainer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Meme name="devs" />, document.getElementById('root'));
+ReactDOM.render(<MemeContainer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
